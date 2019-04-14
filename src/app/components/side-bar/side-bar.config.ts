9232dda2508c20bd41c2b527f1abfcae0e68dc9d
Tran Thanh <tranthanhdeveloper@gmail.com>
2019-04-14 09:51:11 +0700

    Fix left side bar dropdown issue

--- src/app/components/side-bar/side-bar.config.ts
+++ src/app/components/side-bar/side-bar.config.ts
@@ -12,7 +12,7 @@ export const ROUTE = [
     },
     { path: 'charts', title: 'Charts', icon: 'fas fa-chart-pie', children: null },
     { path: 'grids', title: 'Grid Layouts', icon: 'fas fa-th', children: null },
-    { path: '#pages', title: 'Pages', icon: 'far fa-file', children: [
+    { path: '#pages', title: 'Pages', icon: 'far fa-file',  id:'pages', children: [
         {path:'pages/404', title:"404", icon:null, children: null},
         {path:'pages/500', title:"500", icon:null, children: null},
         {path:'pages/blank', title:"Blank", icon:null, children: null}
